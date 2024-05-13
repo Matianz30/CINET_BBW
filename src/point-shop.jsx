c4e99ff695b5dc2f31b99e27d645ed65976f2ef8
Matian Dauti <naitam08@outlook.com>
2024-05-13 16:10:34 +0200

    add point shop items and layout

--- src/point-shop.jsx
+++ src/point-shop.jsx
@@ -4,16 +4,54 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./colors.scss"
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx"
+import {Col, Row} from "react-bootstrap";
 
 function pointshop({points, setPoints}) {
     return (
         <>
             <Header/>
-            <h1>Point shop</h1>
-            <h2>points: {points}</h2>
-
-            <button type="button" className="btn btn-primary"
-                    onClick={event => setPoints(points - 300)}>Product cost: 300</button>
+            <div className="text-center mt-3">
+                <h1>Point shop</h1>
+                <h2 className="mt-2 mb-5">points: {points}</h2>
+                <div className="row m-4">
+                    <div className="m-4 col-sm">
+                        <Col>
+                            <Row className={"d-flex justify-content-center"}>
+                        <img src="popcorn2.jpg" alt="" className="w-75 img-fluid"/>
+                            </Row>
+                            <Row>
+                        <button type="button" className="btn btn-primary img-fluid"
+                                onClick={event => setPoints(points - 300)}>Product cost: 300
+                        </button>
+                            </Row>
+                        </Col>
+                    </div>
+                    <div className="m-4 col-sm">
+                        <Col>
+                            <Row className={"d-flex justify-content-center"}>
+                                <img src="cola.jpg" alt="" className="w-75"/>
+                            </Row>
+                            <Row>
+                                <button type="button" className="btn btn-primary"
+                                        onClick={event => setPoints(points - 300)}>Product cost: 300
+                                </button>
+                            </Row>
+                        </Col>
+                    </div>
+                    <div className="m-4 col-sm">
+                        <Col>
+                            <Row className={"d-flex justify-content-center"}>
+                                <img src="ticket.png" alt="" className="w-75"/>
+                            </Row>
+                            <Row>
+                                <button type="button" className="btn btn-primary"
+                                        onClick={event => setPoints(points - 300)}>Product cost: 300
+                                </button>
+                            </Row>
+                        </Col>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </>
     )
