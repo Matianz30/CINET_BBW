575778d4104d5ff589bfacd5ecb00c8ab7e650a6
Matian Dauti <naitam08@outlook.com>
2024-06-10 09:32:18 +0200

    add stuff

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import PointShop from "./point-shop.jsx";
 import Profile from "./Profile.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import Maze from "../public/game/index.html";
 
 export default function Main() {
     const [points, setPoints] = useState(2000)
@@ -19,12 +20,15 @@ export default function Main() {
     return (
         <BrowserRouter>
             <Header avatar={avatar}/>
+            <div className="mt-5">
             <Routes>
                 <Route path="/" element={<App />}></Route>
                 <Route path="/games" element={<Games />}></Route>
+                <Route path="/games/maze" element={<Maze />}></Route>
                 <Route path="/point-shop" element={<PointShop points={points} setPoints={setPoints} loggedIn={loggedIn}/>}></Route>
                 <Route path="/profile" element={<Profile name={name} lastName={lastName} avatar={avatar}/>}></Route>
             </Routes>
+            </div>
             <Footer name={name} setName={setName} lastName={lastName} setLastName={setLastName} avatar={avatar} setAvatar={setAvatar} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
         </BrowserRouter>
     );
